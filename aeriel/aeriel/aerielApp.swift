dac499a723349efc0600365bab1dc1bc66513dba
Michaël <michael.gainyo@gmail.com>
2023-01-23 18:39:13 +0100

    setup

---
+++ aeriel/aeriel/aerielApp.swift
@@ -0,0 +1,17 @@
+//
+//  aerielApp.swift
+//  aeriel
+//
+//  Created by Michaël Gainyo on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aerielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
